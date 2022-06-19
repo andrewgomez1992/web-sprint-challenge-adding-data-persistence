a792f35f4e786b61f2934003e28dcead356f7008
Andrew Gomez <drewgomez209@gmail.com>
2022-06-18 20:52:30 -0700

    server running

--- api/server.js
+++ api/server.js
@@ -1,1 +1,18 @@
 // build your server here and require it from index.js
+const express = require('express')
+const projectRouter = require('../api/project/router')
+const resourceRouter = require('../api/resource/router')
+const taskRouter = require('../api/task/router')
+
+const server = express()
+
+server.use(express.json())
+// server.use('/api/projects', projectRouter)
+// server.use('/api/resources', resourceRouter)
+server.use('/api/tasks', taskRouter)
+
+server.use((err, req, res, next) => {
+    res.status(err.status || 500).json({ message: err.message })
+})
+
+module.exports = server
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,20 @@
 // build your `/api/tasks` router here
+const express = require('express')
+const helpers = require('./model')
+
+const router = express.Router()
+
+router.get('/tasks', (req, res, next) => {
+    helpers.getTasks()
+        .then(tasks => {
+            res.json(tasks)
+        })
+        .catch(next)
+})
+
+
+
+
+
+
+module.exports = router
